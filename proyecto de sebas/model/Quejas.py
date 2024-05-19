1048be44a0781f6626f8a8cd6ec1453563c853db
NEROrt3317 <160744148+NEROrt3317@users.noreply.github.com>
2024-05-18 22:49:48 -0500

    se adjutna tood el proyecto de sebas

---
+++ proyecto de sebas/model/Quejas.py
@@ -0,0 +1,14 @@
+#import model.Mesas as Mesas
+import Mesas
+class Qjas:
+    def __init__(self,quejas:str,queja:str,queja_mesa) -> None:
+        self.quejas=quejas
+        self.queja=queja
+        self.queja_mesa=queja_mesa
+    def ing_quejas(self):
+        for self.quejas in Mesas:
+            if self.quejas == "Si" or "si":
+                self.queja=str(input("Ingrese la queja: "))
+                self.queja_mesa=int(input("Ingrese la mesa que se quejo"))
+                return self.queja,self.queja_mesa
+            
